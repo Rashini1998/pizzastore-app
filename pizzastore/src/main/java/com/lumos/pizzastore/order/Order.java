aa40cb504e197a4e17d65a863b34c32beac7fcaa
sachla <sach.pushpawela@gmail.com>
2022-11-09 21:26:08 +0530

    first commit

---
+++ pizzastore/src/main/java/com/lumos/pizzastore/order/Order.java
@@ -0,0 +1,78 @@
+package com.lumos.pizzastore.order;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Integer orderId;
+
+    @Column(name = "pizza_id", nullable = false)
+    private String pizzaId;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price", nullable = false)
+    private Float price;
+
+    public Order(){
+    }
+
+    public Order(Integer orderId, String pizzaId, LocalDateTime orderDate, Integer quantity, Float price) {
+        this.orderId = orderId;
+        this.pizzaId = pizzaId;
+        this.orderDate = orderDate;
+        this.quantity = quantity;
+        this.price = price;
+        System.out.println(pizzaId);
+    }
+
+    //Getters and Setters
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getPizzaId() {
+        return pizzaId;
+    }
+
+    public void setPizzaId(String pizzaId) {
+        this.pizzaId = pizzaId;
+    }
+
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+}
